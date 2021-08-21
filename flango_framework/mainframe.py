093498edcb8d39d52cf014dfa4bbfe9eddb570df
Dmitrij <gdems@fastmail.com>
2021-08-21 21:57:38 +0300

    add POST handler

--- flango_framework/mainframe.py
+++ flango_framework/mainframe.py
@@ -1,4 +1,8 @@
 # Это как бы main.py
+import quopri
+from flango_framework.request_handler import TheGetHandler, ThePostHandler
+
+
 class PageMissing():
 
     def __call__(self, request):
@@ -15,6 +19,19 @@ class Flango():
         way = environ['PATH_INFO']
         way += '/' if way[-1] != '/' else ''
         request = {}
+
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = ThePostHandler().get_request_params(environ)
+            request['data'] = data
+            print(f'Нам пришёл POST-запрос: {Flango.decode_value(data)}')
+        if method == 'GET':
+            request_params = TheGetHandler().get_request_params(environ)
+            request['request_params'] = request_params
+            print(f'Нам пришли GET-параметры: {request_params}')
+
         for layer in self.mid_layers:
             layer(request)
         if way in self.router:
@@ -25,3 +42,12 @@ class Flango():
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
