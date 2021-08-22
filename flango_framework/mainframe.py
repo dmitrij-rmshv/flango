11e316d07e930bd92fc62d0b88ff37e5cefd6904
Dmitrij <gdems@fastmail.com>
2021-08-22 15:15:44 +0300

    lesson2 done

--- flango_framework/mainframe.py
+++ flango_framework/mainframe.py
@@ -1,5 +1,6 @@
 # Это как бы main.py
 import quopri
+from time import ctime
 from flango_framework.request_handler import TheGetHandler, ThePostHandler
 
 
@@ -26,7 +27,13 @@ class Flango():
         if method == 'POST':
             data = ThePostHandler().get_request_params(environ)
             request['data'] = data
-            print(f'Нам пришёл POST-запрос: {Flango.decode_value(data)}')
+            decoded_data = Flango.decode_value(data)
+            print(f'Нам пришёл POST-запрос: {decoded_data}')
+            if 'user_msg' in data:
+                with open('user_messages.txt', 'a') as msg_log:
+                    record = '; '.join(
+                        [f'{key.title()}: {value}' for key, value in decoded_data.items()])
+                    msg_log.write(ctime() + record + '\n')
         if method == 'GET':
             request_params = TheGetHandler().get_request_params(environ)
             request['request_params'] = request_params
--- flango_framework/request_handler.py
+++ flango_framework/request_handler.py
@@ -12,23 +12,12 @@ class TheGetHandler:
 class ThePostHandler:
 
     @staticmethod
-    def parse_input_data(data: str):
-        result = {}
-        if data:
-            # делим параметры через &
-            params = data.split('&')
-            for item in params:
-                # делим ключ и значение через =
-                k, v = item.split('=')
-                result[k] = v
-        return result
+    def parse_input_data(data: str) -> dict:
+        return {item.split('=')[0]: item.split('=')[1] for item in data.split("&") if data}
 
     @staticmethod
     def get_wsgi_input_data(env) -> bytes:
-        # получаем длину тела
-        content_length_data = env.get('CONTENT_LENGTH')
-        # приводим к int
-        content_length = int(content_length_data) if content_length_data else 0
+        content_length = int(env.get('CONTENT_LENGTH', 0))
         # считываем данные, если они есть
         data = env['wsgi.input'].read(
             content_length) if content_length > 0 else b''
@@ -44,8 +33,4 @@ class ThePostHandler:
         return result
 
     def get_request_params(self, environ):
-        # получаем данные
-        data = self.get_wsgi_input_data(environ)
-        # превращаем данные в словарь
-        data = self.parse_wsgi_input_data(data)
-        return data
+        return self.parse_wsgi_input_data(self.get_wsgi_input_data(environ))
