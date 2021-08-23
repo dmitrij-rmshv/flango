5c04da7aa9a1c33650a0805de1190307c9a0ad5f
Dmitrij <gdems@fastmail.com>
2021-08-23 22:06:57 +0300

    wsgiref added

--- run_app.py
+++ run_app.py
@@ -1,4 +1,9 @@
+from wsgiref.simple_server import make_server
 from courses_app.links import router, mid_layers
 from flango_framework.mainframe import Flango
 
 application = Flango(router, mid_layers)
+
+with make_server('', 8080, application) as httpd:
+    print("Запуск на порту 8080...")
+    httpd.serve_forever()
